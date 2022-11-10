e1cd7c3112507338286d74ceb73f3dd8ce4a9e59
JoshHumphrey31 <humphrey.joshua31@gmail.com>
2022-11-10 15:17:44 -0800

    Still need bottom bar but working on it

---
+++ lib/search_screen.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Search(),
+));
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  get show => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+//grey bar top of screen and grey background with search icon
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: const Text("Search for event"),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[700],
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(12),
+                        //search icon in the search box
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 15),
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search",
+                            hintStyle: TextStyle(color: Colors.white),
+                            icon: Icon(Icons.search, color: Colors.white)
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+        )
+    );
+  }
+}
+
+
+
+
